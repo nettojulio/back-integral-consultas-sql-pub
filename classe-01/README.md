83f957d91ec5713185025b1cdff3de216ac73cda
Julio Neto <nettojulio@hotmail.com>
2021-09-03 21:34:48 -0300

    Tarefa OK!

--- classe-01/README.md
+++ classe-01/README.md
@@ -62,9 +62,9 @@ Faça commit do resultado.
 
 Preencha a checklist para finalizar o exercício:
 
--   [ ] Resolver o exercício revendo a aula se necessário
--   [ ] Adicionar as mudanças aos commits (`git add .` para adicionar todos os arquivos, ou `git add nome_do_arquivo` para adicionar um arquivo específico)
--   [ ] Commitar a cada mudança significativa ou na finalização do exercício (`git commit -m "Mensagem do commit"`)
--   [ ] Pushar os commits na sua branch na origem (`git push origin nome-da-branch`)
+-   [X] Resolver o exercício revendo a aula se necessário
+-   [X] Adicionar as mudanças aos commits (`git add .` para adicionar todos os arquivos, ou `git add nome_do_arquivo` para adicionar um arquivo específico)
+-   [X] Commitar a cada mudança significativa ou na finalização do exercício (`git commit -m "Mensagem do commit"`)
+-   [X] Pushar os commits na sua branch na origem (`git push origin nome-da-branch`)
 
 ###### tags: `lógica` `módulo 1` `exercício de classe` `matemática` `nodeJS`
--- classe-01/query.sql
+++ classe-01/query.sql
@@ -1,1 +1,18 @@
-
+SELECT id, compositor FROM musicas;
+SELECT composicao, tempo FROM musicas WHERE tempo > 4*60;
+SELECT compositor, composicao FROM musicas WHERE id BETWEEN 47 AND 123;
+SELECT * FROM musicas WHERE compositor IS NOT NULL AND compositor <> 'Bach' AND tempo < 5*60;
+SELECT composicao, tempo FROM musicas WHERE compositor = 'Mozart' OR compositor = 'Bach';
+SELECT * FROM musicas ORDER BY id DESC;
+SELECT * FROM musicas ORDER BY tempo DESC;
+SELECT * FROM musicas ORDER BY tempo ASC LIMIT 5;
+SELECT * FROM musicas ORDER BY tempo DESC LIMIT 10;
+SELECT * FROM musicas ORDER BY tempo ASC LIMIT 10 OFFSET 5;
+SELECT * FROM musicas LIMIT 10 OFFSET 10*3;
+SELECT * FROM musicas LIMIT 12 OFFSET 12*5;
+SELECT DISTINCT compositor FROM musicas WHERE compositor IS NOT NULL;
+SELECT DISTINCT compositor, composicao FROM musicas;
+SELECT * FROM musicas WHERE compositor LIKE 'Bra%';
+SELECT * FROM musicas WHERE ritmo LIKE '%troppo';
+SELECT * FROM musicas WHERE composicao ILIKE '%quartet%';
+SELECT * FROM musicas WHERE composicao NOT ILIKE '%quintet%';
